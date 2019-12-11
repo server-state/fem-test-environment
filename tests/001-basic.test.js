ff152abc13134ec86ccf968c8604c12d0c644e1a
Ludwig Richter <richter@fliegwerk.com>
2019-12-11 20:58:35 +0100

    Added magic var replacement for jest tests

--- tests/001-basic.test.js
+++ tests/001-basic.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { create } from "react-test-renderer";
-import App from "../src/App";
+import App from "../test-src/App";
 
 test('two plus two is four', () => {
     expect(2 + 2).toBe(4);
